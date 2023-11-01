0e4289f1978b5dbed2f0c1103f268035595cd796
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-11-01 19:39:27 +0300

    Connected with the activity

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
@@ -14,10 +16,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToFirst(View view){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
+        FirstFragment firstFragment = new FirstFragment();
+        //fragmentTransaction.add(R.id.frame_layout,firstFragment).commit();
+        fragmentTransaction.replace(R.id.frame_layout,firstFragment).commit();
     }
 
     public void goToSecond(View view){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        SecondFragment secondFragment = new SecondFragment();
+        //fragmentTransaction.add(R.id.frame_layout,secondFragment).commit();
+        fragmentTransaction.replace(R.id.frame_layout,secondFragment).commit();
 
     }
 }
